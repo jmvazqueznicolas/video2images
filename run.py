1d8f475cb2cf35f0d2e5d988d4e2f114a78c8cce
JesusVazquez <jmvazqueznicolas@gmail.com>
2022-05-02 10:16:10 -0500

    Add parser options

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 import glob
 import os
 import shutil
+import argparse
+import time
 
 from video2images import Video2Images
 
+# Define and parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--videos_path", type=str, default=".")
+parser.add_argument("--output_path", type=str, default="images")
+parser.add_argument("--capture_interval", type=int, default=1)
+args = parser.parse_args()
+
+
 def main():
+    path = args.videos_path
     ext = '*.mp4'
-    videos = glob.glob(ext)
+    videos = glob.glob(path + '/' + ext)
 
     isExist = os.path.exists('frames_cropped')
     if not isExist:
@@ -15,14 +26,13 @@ def main():
     for video in videos:
         print(f"Extrayendo de {video}")
         Video2Images(video_filepath=video, capture_rate=1, out_dir="frames_cropped")
+        time.sleep(1)
     
     # Select directories whit images
     subfolders = [f.path for f in os.scandir('frames_cropped') if f.is_dir()]
-    print("Estos son los subfolders", subfolders)
     paths = [f for f in subfolders if f.startswith('frames')]
-    print("Estos son los paths", paths)
 
-    cont = 0
+    cont = 1
     isExist = os.path.exists('images')
     if not isExist:
         os.mkdir('images')
@@ -30,7 +40,8 @@ def main():
     for path in paths:
         all_images = os.listdir(path)
         for f in all_images:
-            os.rename(path + '/' + f, "images/" + f'image_{cont}.jpg')
+            if cont % args.capture_interval == 0:
+                os.rename(path + '/' + f, "images/" + f'image_{cont}.jpg')
             cont += 1
     shutil.rmtree("frames_cropped")
 
